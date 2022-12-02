c58be4fa4c34eed7773e566854d421d87d4b53ea
Crow <5488190+echocrow@users.noreply.github.com>
2022-12-02 16:43:31 +0100

    add 2022-02

---
+++ src/2022/02/1.ts
@@ -0,0 +1,51 @@
+import readLines from '../../lib/readLines.js'
+
+const scores = {
+  win: 6,
+  draw: 3,
+}
+
+type Item = 'rock' | 'paper' | 'scissors'
+
+const items = {
+  A: 'rock',
+  B: 'paper',
+  C: 'scissors',
+  X: 'rock',
+  Y: 'paper',
+  Z: 'scissors',
+} satisfies Record<string, Item>
+type ItemKey = keyof typeof items
+
+const outcomes: Record<Item, Partial<Record<Item, number>>> = {
+  rock: {
+    rock: scores.draw,
+    paper: scores.win,
+  },
+  paper: {
+    paper: scores.draw,
+    scissors: scores.win,
+  },
+  scissors: {
+    rock: scores.win,
+    scissors: scores.draw,
+  },
+}
+
+const itemScores: Record<Item, number> = {
+  rock: 1,
+  paper: 2,
+  scissors: 3,
+}
+
+let score = 0
+for await (const line of readLines(__dirname)) {
+  const [oppItmKey, myItmKey] = line.split(' ') as [ItemKey, ItemKey]
+  const oppItm = items[oppItmKey]
+  const myItm = items[myItmKey]
+
+  score += outcomes[oppItm][myItm] ?? 0
+  score += itemScores[myItm] ?? 0
+}
+
+console.log(score)
---
+++ src/2022/02/2.ts
@@ -0,0 +1,53 @@
+import readLines from '../../lib/readLines.js'
+
+type Item = 'rock' | 'paper' | 'scissors'
+
+const items = {
+  A: 'rock',
+  B: 'paper',
+  C: 'scissors',
+} satisfies Record<string, Item>
+type ItemKey = keyof typeof items
+
+const OpScores = {
+  X: 0,
+  Y: 3,
+  Z: 6,
+} as const
+type Op = keyof typeof OpScores
+
+const outcomes: Record<Item, Record<Op, Item>> = {
+  rock: {
+    X: 'scissors',
+    Y: 'rock',
+    Z: 'paper',
+  },
+  paper: {
+    X: 'rock',
+    Y: 'paper',
+    Z: 'scissors',
+  },
+  scissors: {
+    X: 'paper',
+    Y: 'scissors',
+    Z: 'rock',
+  },
+}
+
+const itemScores: Record<Item, number> = {
+  rock: 1,
+  paper: 2,
+  scissors: 3,
+}
+
+let score = 0
+for await (const line of readLines(__dirname)) {
+  const [oppItmKey, myOp] = line.split(' ') as [ItemKey, Op]
+  const oppItm = items[oppItmKey]
+  const myItm = outcomes[oppItm][myOp]
+
+  score += OpScores[myOp] ?? 0
+  score += itemScores[myItm] ?? 0
+}
+
+console.log(score)
