9141c8294a60cd42f63627231d37e99736416978
Crow <5488190+echocrow@users.noreply.github.com>
2022-12-04 15:46:40 +0100

    add 2021-02

---
+++ src/2021/02/1.ts
@@ -0,0 +1,23 @@
+import IO from 'lib/io.js'
+
+const io = new IO()
+
+const dirs = {
+  x: {
+    forward: 1,
+  } as Record<string, number>,
+  y: {
+    down: 1,
+    up: -1,
+  } as Record<string, number>,
+}
+
+let x = 0
+let y = 0
+for await (const line of io.readLines()) {
+  const [inst, num] = line.split(' ') as [string, number]
+  x += num * (dirs.x[inst] ?? 0)
+  y += num * (dirs.y[inst] ?? 0)
+}
+
+io.write(x * y)
---
+++ src/2021/02/2.ts
@@ -0,0 +1,28 @@
+import IO from 'lib/io.js'
+
+const io = new IO()
+
+class Submarine {
+  x = 0
+  y = 0
+  aim = 0
+  down(n: number) {
+    this.aim += n
+  }
+  up(n: number) {
+    this.aim -= n
+  }
+  forward(n: number) {
+    this.x += n
+    this.y += this.aim * n
+  }
+}
+type Inst = 'down' | 'up' | 'forward'
+
+const sub = new Submarine()
+for await (const line of io.readLines()) {
+  const [inst, num] = line.split(' ') as [Inst, string]
+  sub[inst](Number(num))
+}
+
+io.write(sub.x * sub.y)
