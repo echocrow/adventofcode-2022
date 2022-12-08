5d15b2e0bce71a1912a01f26f995dcd67accee9c
Crow <5488190+echocrow@users.noreply.github.com>
2022-12-08 01:00:39 +0100

    add 2022-07

---
+++ src/2022/07/1.ts
@@ -0,0 +1,42 @@
+import IO from 'lib/io.js'
+import {bigSum} from 'lib/sum.js'
+
+const io = new IO()
+
+function* joinPaths(dirs: string[]) {
+  yield '/'
+  let path = ''
+  for (const dir of dirs) {
+    path += `/${dir}`
+    yield path
+  }
+}
+
+const zero = BigInt(0)
+const cdPrefix = '$ cd '
+const fileRe = /^\d+ [\w.]+$/
+
+let pwd: string[] = []
+const dirSizes = new Map<string, bigint>()
+for await (const line of io.readLines()) {
+  // Handle cd.
+  if (line.startsWith(cdPrefix)) {
+    const path = line.slice(cdPrefix.length)
+    if (path === '/') pwd = []
+    else if (path === '..') pwd.pop()
+    else pwd.push(path)
+  }
+  // Handle file.
+  else if (fileRe.test(line)) {
+    const [sizeStr = ''] = line.split(' ', 1)
+    const size = BigInt(sizeStr)
+    for (const path of joinPaths(pwd)) {
+      dirSizes.set(path, (dirSizes.get(path) ?? zero) + size)
+    }
+  }
+}
+
+const THRESHOLD = BigInt(100_000)
+const total = bigSum([...dirSizes.values()].filter((s) => s <= THRESHOLD))
+
+io.write(total.toString())
---
+++ src/2022/07/2.ts
@@ -0,0 +1,49 @@
+import IO from 'lib/io.js'
+
+const io = new IO()
+
+function* joinPaths(dirs: string[]) {
+  yield '/'
+  let path = ''
+  for (const dir of dirs) {
+    path += `/${dir}`
+    yield path
+  }
+}
+
+const zero = BigInt(0)
+const cdPrefix = '$ cd '
+const fileRe = /^\d+ [\w.]+$/
+
+let pwd: string[] = []
+const dirSizes = new Map<string, bigint>()
+for await (const line of io.readLines()) {
+  // Handle cd.
+  if (line.startsWith(cdPrefix)) {
+    const path = line.slice(cdPrefix.length)
+    if (path === '/') pwd = []
+    else if (path === '..') pwd.pop()
+    else pwd.push(path)
+  }
+  // Handle file.
+  else if (fileRe.test(line)) {
+    const [sizeStr = ''] = line.split(' ', 1)
+    const size = BigInt(sizeStr)
+    for (const path of joinPaths(pwd)) {
+      dirSizes.set(path, (dirSizes.get(path) ?? zero) + size)
+    }
+  }
+}
+
+const TOTAL_SPACE = BigInt(70_000_000)
+const THRESHOLD = BigInt(30_000_000)
+
+const usedSpace = dirSizes.get('/') ?? zero
+const neededSpace = THRESHOLD - (TOTAL_SPACE - usedSpace)
+
+const maxOkDirSize = [...dirSizes.values()].reduce(
+  (max, size) => (size >= neededSpace && size < max ? size : max),
+  TOTAL_SPACE,
+)
+
+io.write(maxOkDirSize.toString())
