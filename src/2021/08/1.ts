6dfc2196045d5d87b2662bcd717dbe59e31d6a34
Crow <5488190+echocrow@users.noreply.github.com>
2022-12-08 03:31:37 +0100

    add 2021-08

---
+++ src/2021/08/1.ts
@@ -0,0 +1,13 @@
+import IO from 'lib/io.js'
+
+const io = new IO()
+
+let result = 0
+const UNIQUE_LENS = new Set([2, 3, 4, 7])
+for await (const line of io.readLines()) {
+  const [_, outStr = ''] = line.split(' | ', 2)
+  const outs = outStr.split(' ')
+  result += outs.filter((s) => UNIQUE_LENS.has(s.length)).length
+}
+
+io.write(result)
---
+++ src/2021/08/2.ts
@@ -0,0 +1,91 @@
+import IO from 'lib/io.js'
+
+const io = new IO()
+
+function isSubset<T>(sub: Set<T>, sup: Set<T>): boolean {
+  if (sub.size > sup.size) return false
+  for (const v of sub) if (!sup.has(v)) return false
+  return true
+}
+
+function isSuperset<T>(sup: Set<T>, sub: Set<T>): boolean {
+  return isSubset(sub, sup)
+}
+
+const A_CHAR = 'a'.charCodeAt(0)
+
+class Digit {
+  rods: Set<string>
+  num = NaN
+  len: number
+  id: number
+  str: string
+
+  constructor(rods: string) {
+    this.str = rods
+    this.len = rods.length
+    this.rods = new Set(rods)
+    this.id = Digit.idFromStr(rods)
+  }
+
+  isPending(): boolean {
+    return isNaN(this.num)
+  }
+
+  solve(num: number) {
+    this.num = num
+    return this
+  }
+
+  static idFromStr(rods: string): number {
+    return [...rods].reduce(
+      (id, c) => id + (1 << (c.charCodeAt(0) - A_CHAR)),
+      0,
+    )
+  }
+}
+
+let result = 0
+for await (const line of io.readLines()) {
+  const [inStr = '', outStr = ''] = line.split(' | ', 2)
+  const ins = inStr.split(' ').map((s) => new Digit(s))
+
+  const digits = Array<Digit>(10)
+
+  function solveNext(
+    forN: number,
+    len: number,
+    condition?: (d: Digit) => boolean,
+  ): Digit {
+    const digit = ins.find(
+      (d) => d.len === len && d.isPending() && (condition?.(d) ?? true),
+    )
+    if (!digit) throw 'not found'
+    digit.solve(forN)
+    digits[forN] = digit
+    return digit
+  }
+
+  solveNext(1, 2)
+  solveNext(4, 4)
+  solveNext(7, 3)
+  solveNext(8, 7)
+
+  solveNext(9, 6, (d) => isSuperset(d.rods, digits[4]!.rods))
+  solveNext(0, 6, (d) => isSuperset(d.rods, digits[1]!.rods))
+  solveNext(6, 6)
+
+  solveNext(3, 5, (d) => isSuperset(d.rods, digits[1]!.rods))
+  solveNext(5, 5, (d) => isSubset(d.rods, digits[6]!.rods))
+  solveNext(2, 5)
+
+  const digitsById = new Map(digits.map((d) => [d.id, d]))
+  const outs = outStr
+    .split(' ')
+    .map((s) => digitsById.get(Digit.idFromStr(s))?.num ?? 0)
+
+  const out = outs.reduce((out, n) => out * 10 + n, 0)
+  result += out
+}
+
+io.write(result)
