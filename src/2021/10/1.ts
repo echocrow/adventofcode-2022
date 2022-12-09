698b06096b730939d3ef058425ca526fee3d3750
Crow <5488190+echocrow@users.noreply.github.com>
2022-12-09 23:50:41 +0100

    add 2021-10

---
+++ src/2021/10/1.ts
@@ -0,0 +1,39 @@
+import IO from 'lib/io.js'
+
+const io = new IO()
+
+const PAIRS: Record<string, string> = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+  '<': '>',
+}
+const SCORES: Record<string, number> = {
+  ')': 3,
+  ']': 57,
+  '}': 1197,
+  '>': 25137,
+}
+
+function validate(str: string): string {
+  const closers: string[] = []
+  for (const c of str) {
+    const closer = closers[closers.length - 1] ?? ''
+    if (c in PAIRS) {
+      closers.push(PAIRS[c]!)
+    } else if (c === closer) {
+      closers.pop()
+    } else {
+      return c
+    }
+  }
+  return ''
+}
+
+let result = 0
+for await (const line of io.readLines()) {
+  const invalid = validate(line)
+  result += SCORES[invalid] ?? 0
+}
+
+io.write(result)
---
+++ src/2021/10/2.ts
@@ -0,0 +1,43 @@
+import IO from 'lib/io.js'
+import sort from 'lib/sort.js'
+
+const io = new IO()
+
+const PAIRS: Record<string, string> = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+  '<': '>',
+}
+const SCORES: Record<string, number> = {
+  ')': 1,
+  ']': 2,
+  '}': 3,
+  '>': 4,
+}
+
+function validate(str: string): string[] {
+  const closers: string[] = []
+  for (const c of str) {
+    const closer = closers[closers.length - 1] ?? ''
+    if (c in PAIRS) {
+      closers.push(PAIRS[c]!)
+    } else if (c === closer) {
+      closers.pop()
+    } else {
+      return []
+    }
+  }
+  return closers.reverse()
+}
+
+const scores: number[] = []
+for await (const line of io.readLines()) {
+  const closers = validate(line)
+  const score = closers.reduce((s, c) => s * 5 + (SCORES[c] ?? 0), 0)
+  if (score) scores.push(score)
+}
+
+sort(scores)
+const result = scores[(scores.length - 1) / 2] ?? 0
+io.write(result)
