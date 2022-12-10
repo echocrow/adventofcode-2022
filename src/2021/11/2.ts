a7d4ec14bd02cee9303a58ab0e195348db3bf021
Crow <5488190+echocrow@users.noreply.github.com>
2022-12-10 01:10:31 +0100

    add 2021-11

---
+++ src/2021/11/2.ts
@@ -0,0 +1,60 @@
+import IO from 'lib/io.js'
+
+const io = new IO()
+
+function merge(a: Uint8Array, b: Uint8Array): Uint8Array {
+  const ab = new Uint8Array(a.length + b.length)
+  ab.set(a)
+  ab.set(b, a.length)
+  return ab
+}
+
+function neighborIdx(arr: Uint8Array, w: number, i: number): number[] {
+  const h = arr.length / w
+  const x = i % w
+  const y = (i - x) / w
+  const left = x ? -1 : 0
+  const right = x < w - 1 ? +1 : 0
+  const above = y ? -w : 0
+  const below = y < h - 1 ? w : 0
+  return [
+    left,
+    right,
+    above,
+    below,
+    left && above ? left + above : 0,
+    left && below ? left + below : 0,
+    right && above ? right + above : 0,
+    right && below ? right + below : 0,
+  ]
+    .filter(Boolean)
+    .map((d) => d + i)
+}
+
+function tick(wales: Uint8Array, width: number) {
+  for (const w in wales) wales[w]++
+  let f: number
+  while ((f = wales.findIndex((w) => w > 9)) >= 0) {
+    wales[f] = 0
+    for (const n of neighborIdx(wales, width, f)) {
+      if (wales[n]! > 0 && wales[n]! <= 9) wales[n]++
+    }
+  }
+}
+
+let wales = new Uint8Array()
+let w = 0
+for await (const line of io.readLines()) {
+  wales = merge(wales, new Uint8Array([...line].map(Number)))
+  w = line.length
+}
+
+let steps = 0
+let inSync = false
+while (!inSync) {
+  tick(wales, w)
+  inSync = wales.every((w) => w === 0)
+  steps++
+}
+
+io.write(steps.toString())
