fdb0f4b3036cfa6e884bc76e654fc0ef9783f2bc
Crow <5488190+echocrow@users.noreply.github.com>
2022-12-13 00:55:43 +0100

    allow reverse sorting

--- src/lib/sort.ts
+++ src/lib/sort.ts
@@ -1,4 +1,8 @@
-export default function sort<N extends number | bigint>(nums: N[]) {
-  nums.sort((a, b) => (a > b ? 1 : a < b ? -1 : 0))
+export default function sort<N extends number | bigint>(
+  nums: N[],
+  dec = false,
+) {
+  const f = dec ? -1 : 1
+  nums.sort((a, b) => (a > b ? 1 : a < b ? -1 : 0) * f)
   return nums
 }
