113147a07833e781d95b5e87f1b14a09c186d811
Crow <5488190+echocrow@users.noreply.github.com>
2022-12-13 00:09:33 +0100

    allow reading individual line

--- src/lib/io.ts
+++ src/lib/io.ts
@@ -1,12 +1,14 @@
 import {createReadStream, writeFileSync, openSync} from 'node:fs'
 import path from 'node:path'
-import {createInterface} from 'node:readline'
+import {createInterface, type Interface} from 'node:readline'
 
 type WriteData = Parameters<typeof writeFileSync>[1]
 
 export default class IO {
   #inPath: string
   #outPath: string
+
+  #inReader: Interface | undefined
   #outFileDesc: number | undefined
 
   constructor(dir = './io', input = 'in.txt', output = 'out.txt') {
@@ -14,13 +16,21 @@ export default class IO {
     this.#outPath = path.join(dir, output)
   }
 
-  async *readLines(rows = 1): AsyncGenerator<string, void, undefined> {
-    const reader = createInterface({
+  get #reader() {
+    return (this.#inReader ??= createInterface({
       input: createReadStream(this.#inPath),
-    })
+    }))
+  }
+
+  async readLine(): Promise<string> {
+    const gen = this.#reader[Symbol.asyncIterator]()
+    return (await gen.next()).value ?? ''
+  }
+
+  async *readLines(rows = 1): AsyncGenerator<string, void, undefined> {
     let r = 0
     let buff = ''
-    for await (const row of reader) {
+    for await (const row of this.#reader) {
       if (buff) buff += '\n'
       buff += row
       r++
@@ -36,11 +46,8 @@ export default class IO {
   async *readRegExp(
     pattern: RegExp,
   ): AsyncGenerator<RegExpExecArray, void, undefined> {
-    const reader = createInterface({
-      input: createReadStream(this.#inPath),
-    })
     let buff = ''
-    for await (const row of reader) {
+    for await (const row of this.#reader) {
       buff += (buff ? '\n' : '') + row
       const res = pattern.exec(buff)
       if (res) {
