c89bf9967a8e6ce7488bd6b85f2f10b0416d4efe
Crow <5488190+echocrow@users.noreply.github.com>
2023-12-09 03:08:14 +0100

    re-run file in watch-mode when io in.txt changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,16 @@
+import {join, relative} from 'node:path'
+import {defineConfig, normalizePath} from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default {
-  plugins: [tsconfigPaths()],
-}
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    {
+      name: 'vite-aoc-watch-io-in',
+      transform(_, id) {
+        if (normalizePath(relative(__dirname, id)) === 'src/lib/io.ts')
+          this.addWatchFile(join(__dirname, 'io/in.txt'))
+      },
+    },
+  ],
+})
