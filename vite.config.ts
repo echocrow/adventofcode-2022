fece1a21ed4f980fe2db354fae3b6d7b72ffb2cc
Crow <5488190+echocrow@users.noreply.github.com>
2023-12-10 01:37:44 +0100

    skip test rerun on io file changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import {join, relative} from 'node:path'
-import {defineConfig, normalizePath} from 'vite'
+import {defineConfig, normalizePath, type ResolvedConfig} from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+
+let viteCfg: ResolvedConfig
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     {
-      name: 'vite-aoc-watch-io-in',
+      name: 'vite-plugin-current-config',
+      configResolved: (config) => void (viteCfg = config),
+    },
+    {
+      name: 'vite-plugin-aoc-watch-io-in',
       transform(_, id) {
+        if (viteCfg.mode === 'test') return
         if (normalizePath(relative(__dirname, id)) === 'src/lib/io.ts')
           this.addWatchFile(join(__dirname, 'io/in.txt'))
       },
