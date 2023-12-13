5b274143e33382b8c1ae6bcba95dd60865b437eb
Crow <5488190+echocrow@users.noreply.github.com>
2023-12-13 02:58:40 +0100

    add memo helpers with disposable symbol

--- src/lib/memo.ts
+++ src/lib/memo.ts
@@ -1,8 +1,36 @@
-export default function memoize<F extends (arg: any) => any>(fn: F): F {
-  type Arg = Parameters<F>[0]
-  const memo = new Map<Arg, ReturnType<F>>()
-  return ((arg: Arg) => {
-    if (!memo.has(arg)) memo.set(arg, fn(arg))
-    return memo.get(arg) as ReturnType<F>
-  }) as F
+type DisposeFn = Disposable[typeof Symbol.dispose]
+
+export default function memoize<TArgs extends [any], TRes>(
+  fn: (...args: TArgs) => TRes,
+): ((...args: TArgs) => TRes) & Disposable {
+  return memoizeWithKey(fn, (...args) => args[0])
+}
+
+export function memoizeWithKey<TArgs extends any[], TRes, TKey>(
+  fn: (...args: TArgs) => TRes,
+  getKey: (...args: TArgs) => TKey,
+): ((...args: TArgs) => TRes) & Disposable {
+  const memo = new Map<TKey, TRes>()
+  return memoizeWithStorage(fn, getKey, memo)
+}
+
+export function memoizeWithStorage<TArgs extends any[], TRes, TKey>(
+  fn: (...args: TArgs) => TRes,
+  getKey: (...args: TArgs) => TKey,
+  memo: {
+    has: (key: TKey) => boolean
+    get: (key: TKey) => TRes | undefined
+    set: (key: TKey, val: TRes) => void
+    clear?: () => void
+  },
+): ((...args: TArgs) => TRes) & Disposable {
+  const memFn = (...args: TArgs): TRes => {
+    const key = getKey(...args)
+    if (memo.has(key)) return memo.get(key)!
+    const res = fn(...args)
+    memo.set(key, res)
+    return res
+  }
+  memFn[Symbol.dispose] = (() => memo.clear?.()) as DisposeFn
+  return memFn
 }
