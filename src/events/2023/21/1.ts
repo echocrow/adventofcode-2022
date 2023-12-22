c21762ac78c0a25236ffe594ea381a2fdfd53349
Crow <5488190+echocrow@users.noreply.github.com>
2023-12-22 15:29:20 +0100

    add 2023-21 part 1

---
+++ src/events/2023/21/1.ts
@@ -0,0 +1,32 @@
+import io from '#lib/io.js'
+import {Uint8Matrix, neighbors} from '#lib/matrix.js'
+
+const STEPS = Number((await io.readLineIfMatch(/^__steps=(\d+)$/))?.[1] ?? 64)
+
+const garden = new Uint8Matrix()
+let startI = 0
+for await (const line of io.readLines()) {
+  const startCol = line.indexOf('S')
+  if (startCol >= 0) startI = garden.length + startCol
+  garden.pushRow(line.split('').map((c) => +(c === '#')))
+}
+
+let queue = [startI]
+let parityOk = STEPS % 2 === 0
+let result = +parityOk
+garden.$[startI] = 1
+for (let s = 1; s <= STEPS; s++) {
+  parityOk = !parityOk
+  let nextQueue: typeof queue = []
+  for (const currI of queue) {
+    for (const i of neighbors(garden, currI)) {
+      if (garden.$[i]) continue
+      if (parityOk) result++
+      garden.$[i] = 1
+      nextQueue.push(i)
+    }
+  }
+  queue = nextQueue
+}
+
+io.write(result)
