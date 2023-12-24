e5740d2cb165d41be5bba5eb333d4158b30a636e
Crow <5488190+echocrow@users.noreply.github.com>
2023-12-24 01:36:51 +0100

    add 2023-21 part 2

---
+++ src/events/2023/21/2.ts
@@ -0,0 +1,108 @@
+import io from '#lib/io.js'
+import {map, sum} from '#lib/iterable.js'
+import {Uint8Matrix, neighbors} from '#lib/matrix.js'
+
+const STEPS = Number((await io.readCfgLine('__steps')) ?? 26501365)
+
+const garden = new Uint8Matrix()
+for await (const line of io.readLines())
+  garden.pushRow(line.split('').map((c) => +(c === '#')))
+
+function countStepsFrom(x: number, y: number) {
+  const counts: number[] = []
+
+  const startI = garden.vecToI(x, y)
+  const visited = new Uint8Matrix(garden)
+
+  let queue = [startI]
+  let isOdd = false
+  let evens = 1
+  let odds = 0
+  visited.$[startI] = 1
+  counts.push(isOdd ? odds : evens)
+  while (queue.length) {
+    isOdd = !isOdd
+    let nextQueue: typeof queue = []
+    for (const currI of queue) {
+      for (const i of neighbors(visited, currI)) {
+        if (visited.$[i]) continue
+        isOdd ? odds++ : evens++
+        visited.$[i] = 1
+        nextQueue.push(i)
+      }
+    }
+    queue = nextQueue
+    counts.push(isOdd ? odds : evens)
+  }
+  return counts
+}
+
+// We assume the garden is square and odd-sized.
+// Additionally, we assume the start is at the center of the garden.
+// Lastly, we assume that we have a clean in the center lanes.
+if (garden.width !== garden.height) throw new Error('Square-gardens only!')
+if (garden.width % 2 === 0) throw new Error('Odd-sized gardens only!')
+
+const length = garden.width
+const start = 0
+const center = Math.floor(length / 2)
+const end = length - 1
+
+const startCounts = countStepsFrom(center, center)
+
+function floorOdd(num: number) {
+  return num - (1 - (num % 2))
+}
+function floorEven(num: number) {
+  return num - (num % 2)
+}
+function floorParity(num: number, toOdd: boolean | number) {
+  return toOdd ? floorOdd(num) : floorEven(num)
+}
+
+const cornersStepCounts = [
+  countStepsFrom(center, start),
+  countStepsFrom(center, end),
+  countStepsFrom(start, center),
+  countStepsFrom(end, center),
+]
+const diagsStepCounts = [
+  countStepsFrom(start, start),
+  countStepsFrom(end, start),
+  countStepsFrom(start, end),
+  countStepsFrom(end, end),
+]
+function getStepCount(stepCounts: number[][], steps: number) {
+  return steps > 0 ? sum(map(stepCounts, (counts) => counts[steps - 1]!)) : 0
+}
+
+const PARITY = STEPS % 2
+
+// Full tiles.
+const fullDiamondLen = Math.floor(STEPS / length)
+const evenFullTiles = floorOdd(fullDiamondLen) ** 2
+const oddFullTiles = floorEven(fullDiamondLen) ** 2
+const evenFullStepCount =
+  startCounts[Math.min(floorParity(startCounts.length - 1, PARITY), STEPS)]!
+const oddFullStepCount =
+  startCounts[floorParity(startCounts.length - 1, 1 - PARITY)]!
+
+// Corners.
+const innerCornerTileSteps = STEPS > length ? (STEPS % length) + center + 1 : 0
+const outerCornerTileSteps = (STEPS % length) - center
+
+// Diagonals.
+const evenDiagTileSteps = (STEPS - length) % (length * 2)
+const oddDiagTileSteps = (STEPS - length * 2) % (length * 2)
+const evenDiagTiles = Math.ceil((STEPS - length) / (length * 2)) * 2 - 1
+const oddDiagTiles = Math.ceil((STEPS - length * 2) / (length * 2)) * 2
+
+const result = sum([
+  evenFullStepCount * evenFullTiles,
+  oddFullStepCount * oddFullTiles,
+  getStepCount(cornersStepCounts, innerCornerTileSteps),
+  getStepCount(cornersStepCounts, outerCornerTileSteps),
+  getStepCount(diagsStepCounts, evenDiagTileSteps) * evenDiagTiles,
+  getStepCount(diagsStepCounts, oddDiagTileSteps) * oddDiagTiles,
+])
+io.write(result)
---
+++ src/events/2023/21/2visualize.ts
@@ -0,0 +1,103 @@
+import {createInterface} from 'node:readline/promises'
+import io from '#lib/io.js'
+import {Uint8Matrix, neighbors} from '#lib/matrix.js'
+import {addVec2, scaleVec2} from '#lib/vec2.js'
+
+// Config.
+const MAX_STEPS = Number((await io.readCfgLine('__steps')) ?? Infinity)
+const SCALE_RADIUS = Number((await io.readCfgLine('__scale')) ?? 5)
+const INTERACTIVE = !!(await io.readCfgLine('__interactive'))
+const SHOW_REACH_EDGE_ONLY = !!(await io.readCfgLine('__edgesOnly'))
+const SEPARATE = true
+
+// Parse input.
+const srcGarden = new Uint8Matrix()
+let srcStartI = 0
+for await (const line of io.readLines()) {
+  const startCol = line.indexOf('S')
+  if (startCol >= 0) srcStartI = srcGarden.length + startCol
+  srcGarden.pushRow(line.split('').map((c) => +(c === '#')))
+}
+
+// Scale garden.
+const scale = SCALE_RADIUS * 2 + 1
+const garden = new Uint8Matrix(
+  srcGarden.length * scale ** 2,
+  srcGarden.width * scale,
+)
+const startI = garden.vecToI(
+  ...addVec2(
+    srcGarden.iToVec(srcStartI),
+    scaleVec2([srcGarden.width, srcGarden.height], SCALE_RADIUS),
+  ),
+)
+for (let y = 0; y < srcGarden.height; y++) {
+  const row = new Uint8Array(srcGarden.row(y))
+  for (let cy = 0; cy < scale; cy++) {
+    for (let cx = 0; cx < scale; cx++) {
+      let i = garden.vecToI(srcGarden.width * cx, srcGarden.height * cy + y)
+      garden.$.set(row, i)
+    }
+  }
+}
+
+const visChars = [' ', '#', 'O']
+const ySeparator =
+  ('-'.repeat(srcGarden.width) + '+').repeat(scale - 1) +
+  '-'.repeat(srcGarden.width)
+function fmtGarden(garden: Uint8Matrix) {
+  return garden.fmt((v, i) => {
+    let sep = ''
+    const [x, y] = garden.iToVec(i)
+    if (SEPARATE && !x && y && !(y % srcGarden.height)) sep += ySeparator + '\n'
+    if (SEPARATE && x && !(x % srcGarden.width)) sep += '|'
+    return sep + visChars[v]!
+  })
+}
+
+const rl =
+  INTERACTIVE ?
+    createInterface({input: process.stdin, output: process.stdout})
+  : undefined
+
+const visited = new Uint8Matrix(garden)
+const visitedEven = new Uint8Matrix(garden)
+const visitedOdd = new Uint8Matrix(garden)
+
+let queue = [startI]
+let evens = 0
+let odds = 0
+let step = 0
+{
+  visited.$[startI] = visitedEven.$[startI] = 2
+  evens++
+}
+let isOdd = false
+while (queue.length && step < MAX_STEPS) {
+  step++
+  isOdd = !isOdd
+  let nextQueue: typeof queue = []
+  const newReachGarden = new Uint8Matrix(garden)
+  for (const currI of queue) {
+    for (const i of neighbors(visited, currI)) {
+      if (visited.$[i]) continue
+      visited.$[i] = 2
+      isOdd ? odds++ : evens++
+      ;(isOdd ? visitedOdd : visitedEven).$[i] = 2
+      newReachGarden.$[i] = 2
+      nextQueue.push(i)
+    }
+  }
+  queue = nextQueue
+  if (INTERACTIVE) {
+    const reached = isOdd ? odds : evens
+    const reachedGarden = isOdd ? visitedOdd : visitedEven
+    io.log(fmtGarden(SHOW_REACH_EDGE_ONLY ? newReachGarden : reachedGarden))
+    io.log({step, reached})
+    const res = (await rl?.question('continue? [Y/n] ')) ?? 'n'
+    if (res === 'n') break
+  }
+}
+
+rl?.close()
+io.write(fmtGarden(isOdd ? visitedOdd : visitedEven))
