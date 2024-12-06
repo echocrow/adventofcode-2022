c79c94289cf31bc2611e9cdbc0e7f4686af96396
Crow <5488190+echocrow@users.noreply.github.com>
2024-12-06 01:21:49 +0100

    remove unused helper

--- src/lib/isset.ts
+++
@@ -1,7 +0,0 @@
-export default function isset<T>(thing: T): thing is NonNullable<T> {
-  return (
-    thing !== undefined &&
-    thing !== null &&
-    (typeof thing !== 'number' || !isNaN(thing))
-  )
-}
