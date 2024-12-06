9b5fa0c795c50450b902c679a9dc8839e95e6dce
Crow <5488190+echocrow@users.noreply.github.com>
2024-12-06 01:20:58 +0100

    remove unused str helper

--- src/lib/sliceByLen.ts
+++
@@ -1,9 +0,0 @@
-import type {Sliceable} from './types.js'
-
-export default function sliceByLen<T>(
-  str: Sliceable<T>,
-  start: number,
-  len: number,
-): T {
-  return str.slice(start, start + len)
-}
