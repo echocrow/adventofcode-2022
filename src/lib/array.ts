56a540821d426d649c0521cc894718f0fd820f8e
Crow <5488190+echocrow@users.noreply.github.com>
2024-12-13 01:13:26 +0100

    simplify typed array type

--- src/lib/array.ts
+++ src/lib/array.ts
@@ -25,19 +25,18 @@ export async function arrFromAsync<T>(
   return vals
 }
 
-export type TypedArrayConstructor =
-  | Int8ArrayConstructor
-  | Uint8ArrayConstructor
-  | Uint8ClampedArrayConstructor
-  | Int16ArrayConstructor
-  | Uint16ArrayConstructor
-  | Int32ArrayConstructor
-  | Uint32ArrayConstructor
-  | Float32ArrayConstructor
-  | Float64ArrayConstructor
-  | BigInt64ArrayConstructor
-  | BigUint64ArrayConstructor
-export type TypedArray = InstanceType<TypedArrayConstructor>
+export type TypedArray =
+  | Int8Array
+  | Uint8Array
+  | Uint8ClampedArray
+  | Int16Array
+  | Uint16Array
+  | Int32Array
+  | Uint32Array
+  | Float32Array
+  | Float64Array
+  | BigInt64Array
+  | BigUint64Array
 
 export type AnyArray = TypedArray | Array<any>
 
