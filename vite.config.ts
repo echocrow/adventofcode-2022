a7a72cdc4a5fccd4623d7f3254099b5314cf2719
Crow <5488190+echocrow@users.noreply.github.com>
2024-12-22 23:59:15 +0100

    make vite io watch plugin atomic

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,21 @@ import {join, relative} from 'node:path'
 import {defineConfig, normalizePath, type ResolvedConfig} from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-let viteCfg: ResolvedConfig
-
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    {
-      name: 'vite-plugin-current-config',
-      configResolved: (config) => void (viteCfg = config),
-    },
-    {
-      name: 'vite-plugin-aoc-watch-io-in',
-      transform(_, id) {
-        if (viteCfg.mode === 'test') return
-        if (normalizePath(relative(__dirname, id)) === 'src/lib/io.ts')
-          this.addWatchFile(join(__dirname, 'io/in.txt'))
-      },
-    },
+    (() => {
+      let viteCfg: ResolvedConfig
+      return {
+        name: 'vite-plugin-aoc-watch-io-in',
+        configResolved: (config) => void (viteCfg = config),
+        transform(_, id) {
+          if (viteCfg.mode === 'test') return
+          if (normalizePath(relative(__dirname, id)) === 'src/lib/io.ts')
+            this.addWatchFile(join(__dirname, 'io/in.txt'))
+        },
+      }
+    })(),
   ],
   esbuild: {target: 'es2022'},
   test: {
